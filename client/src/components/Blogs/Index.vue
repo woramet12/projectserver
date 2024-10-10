fec773a1e335638ddd6e52c27c5c14973e4b2480
Pee <worametpee16@gmail.com>
2024-10-10 20:54:40 +0700

    Updated function b

--- client/src/components/Blogs/Index.vue
+++ client/src/components/Blogs/Index.vue
@@ -1,47 +1,49 @@
 <template>
     <div>
         <h2>Get all blogs</h2>
-        <p><button v-on:click="logout">Logout</button></p>
         <h4>จำนวน blog {{blogs.length}}</h4>
         <p><button v-on:click="navigateTo('/blog/create')">สร้าง blog</button></p>
-        <div v-for="blog in blogs" v-bind:key="blog.id">
-            <p>id: {{ blog.id }}</p>
-            <p>title: {{ blog.title }}</p>
-            <p>content: {{ blog.content }}</p>
-            <p>category: {{ blog.category }}</p>
-            <p>status: {{ blog.status }}</p>
-            <p>
-            <button v-on:click="navigateTo('/blog/'+ blog.id)">ดู blog</button>
-            <button v-on:click="navigateTo('/blog/edit/'+ blog.id)">แก้ไข blog</button>
-            <button v-on:click="deleteBlog(blog)">ลบข้อมูล</button>
-            </p>
-            <hr>
+        <div class="content-grid">
+            <div v-for="blog in blogs" v-bind:key="blog.id" class="blog-card">
+                <p>id: {{ blog.id }}</p>
+                <p>title: {{ blog.title }}</p>
+                <p>content: {{ blog.content }}</p>
+                <p>category: {{ blog.category }}</p>
+                <p>status: {{ blog.status }}</p>
+                <p>
+                    <button v-on:click="navigateTo('/blog/' + blog.id)">ดู blog</button>
+                    <button v-on:click="navigateTo('/blog/edit/' + blog.id)">แก้ไข blog</button>
+                    <button v-on:click="deleteBlog(blog)">ลบข้อมูล</button>
+                </p>
+            </div>
         </div>
+        <p><button v-on:click="logout">Logout</button></p>
     </div>
 </template>
+
 <script>
     import BlogsService from '@/services/BlogsService'
     export default {
-        data () {
+        data() {
             return {
                 blogs: []
             }
         },
-        async created () {
+        async created() {
             this.blogs = (await BlogsService.index()).data
         },
         methods: {
-            logout () {
+            logout() {
                 this.$store.dispatch('setToken', null)
                 this.$store.dispatch('setBlog', null)
                 this.$router.push({
                     name: 'login'
                 })
             },
-            navigateTo (route) {
+            navigateTo(route) {
                 this.$router.push(route)
             },
-            async deleteBlog (blog) {
+            async deleteBlog(blog) {
                 let result = confirm("Want to delete?")
                 if (result) {
                     try {
@@ -58,5 +60,29 @@
         }
     }
 </script>
+
 <style scoped>
+/* จัดการให้การ์ดแสดงเป็นตาราง 2x2 */
+.content-grid {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 20px;
+}
+
+/* สไตล์ของการ์ด */
+.blog-card {
+    background-color: #360852;
+    border-radius: 10px;
+    padding: 10px; /* ลด padding เพื่อให้ข้อความใกล้ขอบมากขึ้น */
+    
+    color: #f1c40f;
+    box-sizing: border-box;
+}
+
+/* สำหรับหน้าจอขนาดเล็ก ให้แสดงการ์ดเป็นคอลัมน์เดียว */
+@media (max-width: 768px) {
+    .content-grid {
+        grid-template-columns: 1fr;
+    }
+}
 </style>
--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,62 +1,89 @@
 <template>
   <div>
     <div class="nv-navbar">
+      <!-- ส่วนของโลโก้ -->
+      <img src="https://upload.wikimedia.org/wikipedia/th/thumb/1/12/PL_Logo.svg/1200px-PL_Logo.svg.png" alt="โลโก้" class="nv-logo" />
       <ul class="nav">
-        <li><router-link :to="{ name: 'blogs' }">Blogs</router-link></li>
+        <li><router-link :to="{ name: 'blogs' }">add a football field</router-link></li>
         <li><router-link :to="{ name: 'users' }">Users</router-link></li>
         <li><router-link :to="{ name: 'comments' }">Comments</router-link></li>
-        <!-- if isLogin is true, show logout menu. otherwise, show login menu-->
-        <li><router-link :to="{ name: 'login' }">Login</router-link></li>  
+        <!-- ถ้า isLogin เป็นจริง ให้แสดงเมนูออกจากระบบ มิฉะนั้น ให้แสดงเมนูเข้าสู่ระบบ -->
+        <li v-if="isLogin()">
+          <a @click="logout">Logout</a>
+        </li>
+        <li v-else>
+          <router-link :to="{ name: 'login' }">Login</router-link>
+        </li>
       </ul>
+      <div class="clearfix"></div>
     </div>
   </div>
 </template>
+
 <script>
-
 export default {
-
-  methods:{
-    isLogin(){
+  methods: {
+    isLogin() {
       return this.$store.getters.isUserLoggedIn;
     },
-    logout(){
+    logout() {
       this.$store.dispatch('logout');
-      this.$router.push({name: 'login'});
+      this.$router.push({ name: 'login' });
     }
   }
 };
 </script>
+
 <style scoped>
+/* สไตล์สำหรับแถบนำทาง */
 .nv-navbar {
   background-color: palegoldenrod;
   width: 100%;
-  height: 20px;
-  padding: 10px 0px 10px 0px;
+  padding: 5px 4px; /* ลด padding ด้านซ้ายให้เหลือ 10px */
+  display: flex;
+  align-items: center;
 }
+
+/* สไตล์โลโก้ */
+.nv-logo {
+  height: 35px; /* ลดขนาดความสูงของโลโก้เล็กน้อย */
+  margin-right: 15px;
+}
+
+/* สไตล์เมนูนำทาง */
 .nv-navbar .nav {
   list-style: none;
   margin: 0;
   padding: 0;
-  float: left;
+  display: flex;
+  align-items: center;
 }
+
+/* สไตล์รายการเมนู */
 .nv-navbar .nav li {
-  float: left;
+  margin-left: 3px;
 }
+
+/* สไตล์ลิงก์ในเมนู */
 .nv-navbar .nav li a {
-  padding: 10px;
+  padding: 5px 10px; /* ลด padding เพื่อทำให้ลิงก์ดูสั้นลง */
   text-decoration: none;
   color: gray;
   font-weight: bold;
 }
+
+/* เอฟเฟกต์ hover */
 .nv-navbar .nav li a:hover {
-  padding: 10px;
-  text-decoration: none;
   color: darkslategrey;
 }
+
+/* สไตล์สำหรับลิงก์ที่ active */
 .nv-navbar .nav li a.router-link-active {
   background-color: gold;
   color: darkslategrey;
 }
+
+/* ล้างการลอยขององค์ประกอบ */
 .clearfix {
   clear: left;
 }
