c17f8c773ddb347def0441d8bdc4878c2214edd6
Pee <worametpee16@gmail.com>
2024-10-12 03:11:25 +0700

    change function b

--- projectserver/client/src/services/UploadService.js
+++
@@ -1,9 +0,0 @@
-import Api from '@/services/Api'
-export default {
-    upload(formData) {
-        return Api().post('upload', formData)
-    },
-    delete(picture) {
-        return Api().post('/upload/delete', picture)
-    }
-}
